1117ca34a08a3f65c328ed14563c76df66a93b1d
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-01-22 17:29:42 +0500

    first commit

---
+++ config/settings/local.py
@@ -0,0 +1,10 @@
+from config.settings.base import *
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
